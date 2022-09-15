e768ded927bd9f5707f0e1e64033b9e8a41d4eae
Blessed Joseph <111202318+jorbbey@users.noreply.github.com>
2022-09-15 07:42:04 +0100

    Add files via upload

---
+++ University.js
@@ -0,0 +1,15 @@
+
+
+      // Javascript for Toggle menu
+      
+      var navLinks = document.getElementById("navLinks");
+      
+      function showMenu(){
+        navLinks.style.right = "-2px";
+      }
+      
+      function hideMenu(){
+        navLinks.style.right = "-700px";
+      }
+      
+
